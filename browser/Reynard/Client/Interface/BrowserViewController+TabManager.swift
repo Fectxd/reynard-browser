34bf9b1cc2d58b3cc728b219503f8e4a3b0f023c
Minh Ton <37860569+minh-ton@users.noreply.github.com>
2026-08-09 12:28:21 +0700

    Add more image preview menu options (#298)

--- browser/Reynard/Client/Interface/BrowserViewController+TabManager.swift
+++ browser/Reynard/Client/Interface/BrowserViewController+TabManager.swift
@@ -112,7 +112,14 @@ extension BrowserViewController: TabManagerDelegate {
         if element.type == .image,
            let source = element.srcUri?.trimmingCharacters(in: .whitespacesAndNewlines),
            let url = URL(string: source) {
-            contextMenuCoordinator.present(at: point, target: .image(url), allowsPreview: !element.isMouseInput)
+            let linkURL = element.linkUri
+                .map { $0.trimmingCharacters(in: .whitespacesAndNewlines) }
+                .flatMap { URL(string: $0) }
+            contextMenuCoordinator.present(
+                at: point,
+                target: .image(url, linkURL: linkURL),
+                allowsPreview: !element.isMouseInput
+            )
             return
         }
         
